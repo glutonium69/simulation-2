9626558c78ddcd51466f8b1d882facb7a275407e
tahzib404 <tahzib404@gmail.com>
2024-09-09 14:13:09 +0000

    Making a Shape class for common methods and props

--- src/Classes/Rectangle.ts
+++ src/Classes/Rectangle.ts
@@ -1,110 +1,40 @@
-import { toDeg, toRad } from "../utils";
-import Vector2D from "./Vector2D";
-import World from "./World";
+import { Vertex } from "../utils";
+import Shape from "./Shape";
 
-export default class Rectangle {
-	private _directionalVec: Vector2D;
-	
-	public data = new Map();
-
+export default class Rectangle extends Shape {
 	constructor(
-		private _ctx: CanvasRenderingContext2D,
-		private _x: number,
-		private _y: number,
+		protected ctx: CanvasRenderingContext2D,
+		protected position: Vertex,
 		public width: number,
 		public height: number,
-		public color: string
+		public fillColor: string
 	) {
-		this._directionalVec = new Vector2D(this._x, this._y);
-
+		super(ctx, 4, position, width, height, fillColor);
 	}
 
-	public walk(value: number) {
-		this._directionalVec.walk(value);
-	}
+	protected setVertecies() {
+		const { x, y } = this.directionalVec.getOrigin();
+		const angle = this.rotationalVec.getArgument();
+		const cos = Math.cos(angle);
+		const sin = Math.sin(angle);
+		const halfWidth = this.width / 2;
+		const halfHeight = this.height / 2;
 
-	public lookAt(x: number, y: number) {
-		this._directionalVec.lookAt(x, y);
-	}
-
-	public moveTo(x: number, y: number) {
-		this._directionalVec.moveTo(x, y);
-	}
-
-	public rotate(value: number) {
-		this._directionalVec.rotate(value);
-	}
-
-	public setRotation(angle: number) {
-		this._directionalVec.setRotation(angle);
-	}
-
-	public draw() {
-		if (toDeg(this._directionalVec.getArgument()) === 90)
-			this._drawNormal();
-		else
-			this._drawRotate();
-	}
-
-	public erase() {
-		this._ctx.clearRect(this._x, this._y, this.width, this.height);
-	}
-
-	public isColliding(element: Rectangle | { x: number, y: number }) {
-		if (element instanceof Rectangle) return this._boxCollision(element);
-		return this._pointCollision(element.x, element.y);
-	}
-
-	private _boxCollision(element: Rectangle) {
-		const { x: thisX, y: thisY } = this._getTopLeftPoint();
-		const { x: elemX, y: elemY } = element._getTopLeftPoint();
-
-		return (
-			thisX + this.width >= elemX &&
-			elemX + element.width >= thisX &&
-			thisY + this.height >= elemY &&
-			elemY + element.height >= thisY
-		);
-	}
-
-	private _pointCollision(x: number, y: number) {
-		const { x: thisX, y: thisY } = this._getTopLeftPoint();
-
-		return (
-			x >= thisX &&
-			x <= thisX + this.width &&
-			y >= thisY &&
-			y <= thisY + this.height
-		);
-	}
-
-	private _drawNormal(rotationEnabled = false) {
-		const { x, y } = this._getTopLeftPoint(rotationEnabled);
-		this._ctx.beginPath();
-		this._ctx.fillStyle = this.color;
-		this._ctx.rect(x, y, this.width, this.height);
-		this._ctx.fill();
-	}
-
-	private _drawRotate() {
-		const { x, y } = World.coordWorldToScreen(this._directionalVec.getOrigin());
-		this._ctx.save();
-		this._ctx.translate(x, y);
-		this._ctx.rotate(toRad(90) - this._directionalVec.getArgument());
-		this._drawNormal(true);
-		this._ctx.translate(0, 0);
-		this._ctx.restore();
-	}
-
-	private _getTopLeftPoint(rotationEnabled = false) {
-		if (!rotationEnabled) {
-			const { x, y } = this._directionalVec.getOrigin();
-			return World.coordWorldToScreen(x - this.width / 2, y + this.height / 2);
-		} else {
-			return {
-				x: -this.width / 2,
-				y: -this.height / 2
-			}
-		}
+		this.vertecies[0] = {
+			x: x + halfHeight * cos - halfWidth * sin,
+			y: y + halfHeight * sin + halfWidth * cos
+		};
+		this.vertecies[1] = {
+			x: x - halfHeight * cos - halfWidth * sin,
+			y: y - halfHeight * sin + halfWidth * cos
+		};
+		this.vertecies[2] = {
+			x: x - halfHeight * cos + halfWidth * sin,
+			y: y - halfHeight * sin - halfWidth * cos
+		};
+		this.vertecies[3] = {
+			x: x + halfHeight * cos + halfWidth * sin,
+			y: y + halfHeight * sin - halfWidth * cos
+		};
 	}
 }
---
+++ src/Classes/Shape.ts
@@ -0,0 +1,155 @@
+import { Vertex } from "../utils";
+import Vector2D from "./Vector2D";
+import World from "./World";
+
+export default abstract class Shape {
+    protected vertecies: Vertex[];
+    protected directionalVec: Vector2D;
+    protected rotationalVec: Vector2D = new Vector2D(0, 0);
+    public data = new Map();
+
+    constructor(
+        protected ctx: CanvasRenderingContext2D,
+        public vertexCount: number,
+        protected position: Vertex,
+        public width: number,
+        public height: number,
+        public fillColor: string
+    ) {
+        this.vertecies = new Array<Vertex>(vertexCount);
+        this.directionalVec = new Vector2D(this.position.x, this.position.y);
+        this.setVertecies();
+    }
+
+    protected abstract setVertecies(): void;
+
+    public getPosition() {
+        return this.directionalVec.getOrigin();
+    }
+
+    public getDirection() {
+        return this.directionalVec.getArgument();
+    }
+
+    public moveTo(x: number, y: number) {
+        this.directionalVec.moveTo(x, y);
+        this.setVertecies();
+    }
+
+    public walk(displacement: number) {
+        this.directionalVec.walk(displacement);
+        this.setVertecies();
+    }
+
+    public draw() {
+        this.ctx.beginPath();
+        for (let i = 0; i < this.vertecies.length; i++) {
+            const { x, y } = World.coordWorldToScreen(this.vertecies[i]);
+            if (i === 0) {
+                this.ctx.moveTo(x, y);
+            } else {
+                this.ctx.lineTo(x, y);
+            }
+        }
+        this.ctx.closePath();
+        this.ctx.fillStyle = this.fillColor;
+        this.ctx.fill();
+    }
+
+    public lookAt(x: number, y: number) {
+        this.directionalVec.lookAt(x, y);
+        this.setVertecies();
+    }
+
+    public rotate(angle: number) {
+        this.directionalVec.rotate(angle);
+        this.rotationalVec.rotate(angle);
+        this.setVertecies();
+    }
+
+    public spin(angularVelocity: number) {
+        this.rotationalVec.rotate(angularVelocity);
+        this.setVertecies();
+    }
+
+    public setRotation(angle: number) {
+        this.directionalVec.setRotation(angle);
+        this.rotationalVec.setRotation(angle);
+        this.setVertecies();
+    }
+
+    public isInsideCanvas() {
+        const { x, y } = World.coordWorldToScreen(this._getTopLeft());
+
+        return (
+            x + this.width > 0 &&
+            x < this.ctx.canvas.width &&
+            y + this.width > 0 &&
+            y < this.ctx.canvas.height
+        );
+    }
+
+    public isColliding(target: Shape) {
+        if (this.boundingCircleCollision(target)) {
+            return this.separatingAxisTheorem(target);
+        }
+        return false;
+    }
+
+    public boundingCircleCollision(target: Shape) {
+        const thisPos = this.getPosition();
+        const targetPos = target.getPosition();
+        const thisMaxLength = Math.max(this.width, this.height);
+        const targetMaxLength = Math.max(target.width, target.height);
+        return Math.sqrt(
+            (thisPos.x - targetPos.x) ** 2 +
+            (thisPos.y - targetPos.y) ** 2
+        ) < thisMaxLength / 2 + targetMaxLength / 2;
+    }
+
+    public separatingAxisTheorem(target: Shape) {
+        const allNormals = [...this._computeNormals(this.vertecies), ...this._computeNormals(target.vertecies)];
+
+        for (let i = 0; i < allNormals.length; i++) {
+            const [min1, max1] = this._calculateProjections(this.vertecies, allNormals[i]);
+            const [min2, max2] = this._calculateProjections(target.vertecies, allNormals[i]);
+
+            if (max1 < min2 || max2 < min1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private _computeNormals(vertecies: Vertex[]): Vertex[] {
+        const normals = [];
+        for (let i = 0; i < vertecies.length; i++) {
+            const next = (i + 1) % vertecies.length;
+            const edge = {
+                x: vertecies[next].x - vertecies[i].x,
+                y: vertecies[next].y - vertecies[i].y,
+            };
+            normals.push({ x: -edge.y, y: edge.x });
+        }
+        return normals;
+    }
+
+    private _calculateProjections(vertecies: Vertex[], axis: Vertex): [number, number] {
+        const projectionValues = new Array<number>(vertecies.length);
+
+        for (let i = 0; i < vertecies.length; i++) {
+            projectionValues[i] = vertecies[i].x * axis.x + vertecies[i].y * axis.y;
+        }
+
+        return [Math.min(...projectionValues), Math.max(...projectionValues)];
+    }
+
+    private _getTopLeft(): Vertex {
+        const { x, y } = this.getPosition();
+        return {
+            x: x - this.width / 2,
+            y: y + this.width / 2
+        }
+    }
+}
